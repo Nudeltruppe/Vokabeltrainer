ccf670aaef75d604e863a36ea68db276e7a1ce47
Glowman554 <glowman434@gmail.com>
2023-02-10 08:47:27 +0100

    gui

---
+++ src/benutzerschnittstelle/Benutzerschnittstelle.java
@@ -0,0 +1,49 @@
+package benutzerschnittstelle;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Benutzerschnittstelle extends JFrame
+{
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					Benutzerschnittstelle frame = new Benutzerschnittstelle();
+					frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Benutzerschnittstelle()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		this.contentPane = new JPanel();
+		this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(this.contentPane);
+	}
+
+}
